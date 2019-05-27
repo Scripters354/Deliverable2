0c634167f0e74fe21f8db5500f19da436325c146
Ehimemhen Okharedia <u15120822@tuks.co.za>
2019-05-27 14:45:43 +0200

    MalariaBackend

---
+++ MalariaBackend/Global.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace MalariaBackend
+{
+    public class Global
+    {
+        public static HttpClient WepApiClient = new HttpClient();
+
+        static Global()
+        {
+            WepApiClient.BaseAddress = new Uri("http://localhost:58619/api/");
+            WepApiClient.DefaultRequestHeaders.Clear();
+            WepApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        
+    }
+}
